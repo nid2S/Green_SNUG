4972db4a193fd0be7e25d5c9a92768b91bcc003f
nid2S <s20034@gsm.hs.kr>
2021-11-19 16:01:59 +0900

    preprocesser | emotional conversation

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
 from transformers import TFGPT2LMHeadModel, TFBertModel, GPT2TokenizerFast, BertTokenizerFast
 import tensorflow as tf
+import pandas as pd
 import json
 
 class DatasetGetter:
@@ -9,5 +10,18 @@ class DatasetGetter:
 
     def makeDataset(self):
         # emotional conversation
+        emotional_t = json.load(open("./data/EmotionalConversation/emotional_final_training.json", "r+", encoding="utf-8"))
+        emotional_t = [conv["talk"]["content"] for conv in emotional_t]  # 40827
+        emotional_t = pd.DataFrame.from_dict(emotional_t)
+        emotional_t.to_csv("./data/Traning_dataset/emotional_t.txt", sep="\t", encoding="utf-8")
+
+        emotional_v = json.load(open("./data/EmotionalConversation/emotional_val.json", "r+", encoding="utf-8"))
+        emotional_v = [conv["talk"]["content"] for conv in emotional_v]
+        emotional_v2 = json.load(open("data/EmotionalConversation/emotional_final_val.json", "r+", encoding="utf-8"))
+        emotional_v2 = [conv["talk"]["content"] for conv in emotional_v2 if conv["talk"]["content"] not in emotional_v]
+        emotional_v += emotional_v2  # 9144 + 40007
+        emotional_v = pd.DataFrame.from_dict(emotional_v)
+        emotional_v.to_csv("./data/Validation_dataset/emotional_v.txt", sep="\t", encoding="utf-8")
+
         # korean speech summary
-        pass
+
